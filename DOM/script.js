9d18df93029aa2983b132fd345221f85a56488a5
Avi7877489 <150539120+Avi7877489@users.noreply.github.com>
2024-09-04 20:20:01 +0530

    UPDATE

---
+++ DOM/script.js
@@ -0,0 +1,7 @@
+console.log("Hello World")
+// alert("Code with Avi")
+document.title = " Avi the coder"
+console.log(document.body)
+console.dir(document.body) 
+
+// document.body.childNodes[1]. innerText = "abcd";  39.23
